7c774c6bd33d06421078bfa3bd093afcac49c7a6
Seans Dowdle <sdowdle@student.cccd.edu>
2018-05-08 23:15:39 -0700

    Committing my current code as well as general project structure.

---
+++ NutritionEffex/src/edu/orangecoastcollege/cs272/nutritioneffex/controller/Controller.java
@@ -0,0 +1,64 @@
+package edu.orangecoastcollege.cs272.nutritioneffex.controller;
+import edu.orangecoastcollege.cs272.nutritioneffex.model.*;
+import javafx.collections.ObservableList;
+/**
+ * 
+ * @author our names here...
+ *
+ */
+public class Controller implements AutoCloseable
+{
+	private static Controller theOne;
+	private DBModel mDB;
+	private Controller() {}
+
+	/* ~~~~~~~~~~~~~~~~~~~~~ DIETARY RESTRICTIONS PORTION ~~~~~~~~~~~~~~~~~~~~~ */
+
+	private ObservableList<Food> mAllFoodsList;
+
+	// Constants for the 3 databases
+	private static final String PREFERENCES_DB_NAME = "dietary_preferences.db";
+	private static final String FOODS_DB_NAME = "foods.db";
+	private static final String FAVORITES_DB_NAME = "favorite_foods.db";
+
+	// Dietary Preferences Database
+	private static final String PREFERENCES_TABLE_NAME = "dietary_preferences";
+	private static final String[] PREFERENCES_FIELD_NAMES = { "_id", "preference"};
+	private static final String[] PREFERENCES_FIELD_TYPES = { "INTEGER PRIMARY KEY", "TEXT"};
+
+	// Foods Database
+	private static final String FOODS_TABLE_NAME = "foods";
+	private static final String[] FOODS_FIELD_NAMES = { "_id", "name", "portion_display_name", "vegetables", "fruits", "milk", "meats", "soy",
+														"solid_fats", "added_sugars", "alcohol", "calories", "saturated_fats"};
+	private static final String[] FOODS_FIELD_TYPES = { "INTEGER PRIMARY KEY", "TEXT", "TEXT", "REAL", "REAL", "REAL", "REAL", "REAL", "REAL",
+														"REAL", "REAL", "REAL", "REAL", "REAL", "REAL" };
+	private static final String FOODS_DATA_FILE = "foods.csv";
+
+	// Favorite Foods Database
+	private static final String FAVORITES_TABLE_NAME = "favorite_foods";
+	private static final String[] FAVORITES_FIELD_NAMES = { "_id", "name", "portion_display_name", "solid_fats", "added_sugars", "alcohol", "calories", "saturated_fats"};
+	private static final String[] FAVORITES_FIELD_TYPES = { "INTEGER PRIMARY KEY", "TEXT", "TEXT", "REAL", "REAL", "REAL", "REAL", "REAL", "REAL" };
+
+
+	/* ~~~~~~~~~~~~~~~~~~~~~ END OF DIETARY RESTRICTIONS PORTION ~~~~~~~~~~~~~~~~~~~~~ */
+	
+	
+	/* ~~~~~~~~~~~~~~~~~~~~~ [Insert title] PORTION ~~~~~~~~~~~~~~~~~~~~~ */
+	
+	
+	
+	/* ~~~~~~~~~~~~~~~~~~~~~ END OF [Insert title] PORTION ~~~~~~~~~~~~~~~~~~~~~ */
+	
+	/* ~~~~~~~~~~~~~~~~~~~~~ [Insert title] PORTION ~~~~~~~~~~~~~~~~~~~~~ */
+	
+	
+	
+	/* ~~~~~~~~~~~~~~~~~~~~~ END OF [Insert title] PORTION ~~~~~~~~~~~~~~~~~~~~~ */
+	
+	@Override
+	public void close() throws Exception 
+	{
+		mDB.close();
+	}
+}
+
